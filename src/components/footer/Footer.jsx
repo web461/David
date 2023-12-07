5004edd58cb31716897b13498d77a9b412ffd539
web461 <144519489+web461@users.noreply.github.com>
2023-12-07 16:36:21 +0100

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
                 <a href="#about" className="footer_link">About</a>
             </li>
             <li>
-                <a href="#service" className="footer_link">Projects</a>
+                <a href="#services" className="footer_link">Projects</a>
             </li>
             {/* <li>
                 <a href="#testimonials" className="footer_link">Testimonials</a>
